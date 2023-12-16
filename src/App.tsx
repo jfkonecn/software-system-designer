9cd5e79e6d183ec939dd145d02239763f4dba514
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-16 14:08:03 -0500

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Toolbar from "./toolbar/Toolbar";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen fixed left-0 top-0">
       <div className="h-full w-full grid grid-cols-1 md:grid-cols-[auto,1fr]">
         <Toolbar />
         <Canvas />
